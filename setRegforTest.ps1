ab4ee0d71fa50c309680c2d26da1b05bc89600cd
Edward Thomas <towianisci@hotmail.com>
2021-02-24 13:18:27 -0800

    fixed a string in powershell script

--- setRegforTest.ps1
+++ setRegforTest.ps1
@@ -2,5 +2,5 @@ $testXml = "https://raw.githubusercontent.com/programmingeveryday/IE_EnterpriseM
 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode' -Name SiteList -Value $testXml -Force -Verbose
 
-Set-ItemProperty -Path 'hklm:\SOFTWARE\Policies\Microsoft\Edge -Name InternetExplorerIntegrationSiteList' -Value $testXml -Force -Verbose
+Set-ItemProperty -Path 'hklm:\SOFTWARE\Policies\Microsoft\Edge' -Name InternetExplorerIntegrationSiteList -Value $testXml -Force -Verbose
 Set-ItemProperty -Path 'hklm:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main\EnterpriseMode' -Name SiteList -Value $testXml -Force -Verbose
